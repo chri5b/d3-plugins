255dd5c33e6f9a6c327cfe405d4869794ce6f8e1
Chris Barrett <cmbarrett@gmail.com>
2013-03-10 22:29:08 +0100

    further changes

--- wordtree/wordTree.js
+++ wordtree/wordTree.js
@@ -44,6 +44,8 @@ d3.wordtree = function(){
                 
             var relevantData = extractRelevantData(d);
 
+            var searchTerm = relevantData[0].name.split(' ')[0];
+
             createTree(relevantData,searchTerm,function(error,errorText,treeData,d) {
                 my.treeData(treeData);
 
@@ -151,15 +153,9 @@ d3.wordtree = function(){
         return my;
     }
   
-    my.preTreeData = function(value) {
-        if (!arguments.length) return preTreeData;
-        preTreeData = value;
-        return my;
-    }
-  
-    my.postTreeData = function(value) {
-        if (!arguments.length) return postTreeData;
-        postTreeData = value;
+    my.treeData = function(value) {
+        if (!arguments.length) return treeData;
+        treeData = value;
         return my;
     }
 
@@ -367,7 +363,7 @@ d3.wordtree = function(){
         return tree1;
     }
 
-    function update(source,preOrPost,visualisation,d3TreeLayout) {
+    function update(source,visualisation,d3TreeLayout) {
 
         // Set vertical alignment of tree in the middle of the screen
         source.x0 = my.height() / 2;
@@ -376,21 +372,19 @@ d3.wordtree = function(){
         // Set duration for transitions
         var duration = 500;
 
-        var treeData = prepareTreeData(d3TreeLayout,source,preOrPost); 
+        var treeData = prepareTreeData(d3TreeLayout,source);
         
         // use d3 to bind all the svg g elements which have the 'node' class to the tree data
         var d3NodeData = visualisation.selectAll("g.node")
                 .data(treeData, function(d) { return d.id || (d.id = ++nodeIDCounter); });
 
-        var dragBehaviour = defineDragBehaviour(preOrPost);
-
-        drawNodes(d3NodeData,preOrPost,dragBehaviour,source,duration);
+        drawNodes(d3NodeData,source,duration);
 
         // Use d3 to bind all the svg path elements which have the 'link' class to the tree link data
         var d3LinkData = visualisation.selectAll("path.link")
                 .data(d3TreeLayout.links(treeData), function(d) { return d.target.id; });
 
-        drawLinks(d3LinkData,preOrPost,duration,source);
+        drawLinks(d3LinkData,duration,source);
 
         // Stash the old positions for transition.
         d3NodeData.forEach(function(d) {
@@ -399,7 +393,7 @@ d3.wordtree = function(){
         });
     }
 
-    function prepareTreeData(tree,source,preOrPost) {
+    function prepareTreeData(tree,source) {
          // Compute the new tree layout.
         var nodes = tree.nodes(source).reverse();
         var rootNode = getRoot(nodes[0]);
@@ -430,7 +424,7 @@ d3.wordtree = function(){
                 if(d.depth > my.maxDepth()) {
                     d.visible = false; 
                 } else {
-                    d.y = getYPosition(d,searchTermWidth,depthWidths,preOrPost);
+                    d.y = getYPosition(d,searchTermWidth,depthWidths);
                     d.visible = true;
                     if(d.depth==0) {
                         d.x = my.height()/2;
@@ -608,26 +602,15 @@ d3.wordtree = function(){
         return width + marginForCircle;
     }
 
-    function getYPosition(d,searchTermWidth,depthWidths,preOrPost) {
-        //Calculates horizontal position of this node, given the information about how much space each level in the tree needs
-        
-        // ----------------------------------------- width -----------------------------------------------------------------//
-        // -- margin -- //----------------------- availableSpace ---------------------------------------------//---margin---//
-                        //------- spaceForPreTree ---------//- searchTermWidth -//-- spaceForPostTree --------//
-                        //-- depth[2] --//--- depth [1] ---//- searchTermWidth -//-- depth[1] --//- depth[2] -//
-                        //-------preVisWidth --------------//------------postVisWidth-------------------------//
+    function getYPosition(d,searchTermWidth,depthWidths) {
 
         var yPosition = 0;
             for (var i = d.depth ; i > 0; i--) {
                     yPosition += depthWidths[i];
             }
 
-        if(preOrPost == "pre") {
-            return my.width()/2 - yPosition - searchTermWidth;
-        }
-        else {
             return yPosition;
-        }
+
     }
     
     function getSpaceForTree (searchTermWidth) {
@@ -637,21 +620,21 @@ d3.wordtree = function(){
         return spaceForTree;
     }
 
-    function drawNodes(d3NodeData,preOrPost,dragBehaviour,source,duration) {
-        addNewNodes(d3NodeData,preOrPost,dragBehaviour,source);
+    function drawNodes(d3NodeData,source,duration) {
+        addNewNodes(d3NodeData,source);
         transitionExistingNodes(d3NodeData);
         removeOldNodes(d3NodeData,duration,source);
     }
 
-    function drawLinks(d3LinkData,preOrPost,duration,source) {
+    function drawLinks(d3LinkData,duration,source) {
 
-        addNewLinks(d3LinkData,preOrPost,duration,source);
+        addNewLinks(d3LinkData,duration,source);
         transitionExistingLinks(d3LinkData,duration);
         removeOldLinks(d3LinkData,source,duration);
 
     }
 
-    function addNewNodes(nodes,preOrPost,dragBehaviour,source) {
+    function addNewNodes(nodes,source) {
         
         //Add an svg group with the interactive behaviour and the appropriate position
         var onClickBehaviour = my.onClick();
@@ -667,25 +650,23 @@ d3.wordtree = function(){
         //Add the circle
         nodeEnter.append("svg:circle")
             .attr("r", 1e-6)
-            .call(dragBehaviour)
             .style("fill", function(d) { return d._children ? "lightsteelblue" : "#fff"; });
         
         //Add the text at the appropriate position, size and drag behaviour. id is used to find and highlight the node during drag
         nodeEnter.append("svg:text")
-                .attr("x", function(d) { return (preOrPost == "pre") ? 10 : -10; })
+                .attr("x", 10)
                 .attr("dy", ".35em")
-                .attr("text-anchor", function(d) { return (preOrPost == "pre") ? "start" : "end"; })
+                .attr("text-anchor", "end")
                 .attr("font-size",function(d) { return getFontSize(d.value,my.maxSize(),my.textSizeMultiplier())})
                 .attr("opacity",function(d) { return Math.sqrt(d.value/my.maxSize())>0.25 ? 1 : 0.5 })
-                .attr("id", function(d) { return preOrPost == "pre" ? preOrPost + "-" + d.id : (d.depth == 0 ? "rootnode" : "post-" + d.id); })
-                .text(function(d) { return getNodeText(d,preOrPost);})
-                .call(dragBehaviour)
+                .attr("id", function(d) { return (d.depth == 0 ? "rootnode" : "post-" + d.id); })
+                .text(function(d) { return getNodeText(d);})
                 .style("fill-opacity", 1e-6)
             .append("svg:title")
                 .text(mouseoverText);
     }
 
-    function getNodeText(d,preOrPost) {
+    function getNodeText(d) {
         var result = "";
         var name = "";
         if (d.pluralAndSingular) { //If we have plural and singular forms on the same branch (i.e. ball and balls) then we should show ball(s)
@@ -694,11 +675,8 @@ d3.wordtree = function(){
             name = d.cleanName;
         }
         name = name.replace(/_/g,' '); //substitute any underscores with spaces (for multi-word search terms)
-        if (d.depth == 0 && preOrPost == "pre") { //don't show the search term on the preTree, because its already present on the postTree
-            result = "";
-        } else {
-            result = name;
-        }
+        result = name;
+
         return result;
     }
     
@@ -753,13 +731,13 @@ d3.wordtree = function(){
                 .style("fill-opacity", 1e-6);
     }
 
-    function addNewLinks(d3LinkData,preOrPost,duration,source) {
+    function addNewLinks(d3LinkData,duration,source) {
 
         // Enter any new links at the parent's previous position.
         d3LinkData.enter().insert("svg:path", "g")
                 .attr("class", "link")
                 .attr("opacity", function(d) { return d.target.cleanName == "" ? 0 : 0.2; })
-                .attr("id",function(d) { return preOrPost + "-link-" + d.target.id; })
+                .attr("id",function(d) { return "link-" + d.target.id; })
                 .attr("d", function(d) {
                     var o = {x: source.x0, y: source.y0};
                     return diagonal({source: o, target: o});
